ab5d46fa497df7160d58bca68c94bbc778ea10eb
Anzos65 <andreiopris1@gmail.com>
2023-05-04 19:49:25 +0300

    Main

--- ruffier.py
+++ ruffier.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
 
 app = QApplication([])
 
@@ -16,9 +16,9 @@ win3.setWindowTitle("Ruffier Test")
 win3.setMinimumSize(600,400)
 
 #First window items
-label1 = QLabel("intro")
-label2 = QLabel("long intro text")
-start = QPushButton("Start")
+label1 = QLabel("lorem ipsum dolor sit amet")
+label2 = QLabel("lorem ipsum dolor sit amet")
+start1 = QPushButton("Start")
 vline1 = QVBoxLayout()
 def toWin2():
     win1.hide()
@@ -26,10 +26,54 @@ def toWin2():
 
 vline1.addWidget(label1, alignment = Qt.AlignLeft)
 vline1.addWidget(label2, alignment = Qt.AlignLeft)
-vline1.addWidget(start, alignment = Qt.AlignCenter)
+vline1.addWidget(start1, alignment = Qt.AlignCenter)
 win1.setLayout(vline1)
 
-start.clicked.connect(toWin2)
+#Second window items
+label3 = QLabel("Enter your full name:")
+line1 = QLineEdit("Full name")
+label4 = QLabel("Full years:")
+line2 = QLineEdit("0")
+label5 = QLabel("lorem ipsum dolor sit amet")
+but1 = QPushButton("Start the first test")
+line3 = QLineEdit("0")
+label6 = QLabel("lorem ipsum dolor sit amet")
+but2 = QPushButton("Start doing squats")
+label7 = QLabel("lorem ipsum dolor sit amet")
+but3 = QPushButton("Start the final test")
+line4 = QLineEdit("0")
+line5 = QLineEdit("0")
+start2 = QPushButton("Send the results")
+def toWin3():
+    win2.hide()
+    win3.show()
+
+vline2 = QVBoxLayout()
+vline3 = QVBoxLayout()
+layout1 = QHBoxLayout()
+
+vline2.addWidget(label3, alignment = Qt.AlignLeft)
+vline2.addWidget(line1, alignment = Qt.AlignLeft)
+vline2.addWidget(label4, alignment = Qt.AlignLeft)
+vline2.addWidget(line2, alignment = Qt.AlignLeft)
+vline2.addWidget(label5, alignment = Qt.AlignLeft)
+vline2.addWidget(but1, alignment = Qt.AlignLeft)
+vline2.addWidget(line3, alignment = Qt.AlignLeft)
+vline2.addWidget(label6, alignment = Qt.AlignLeft)
+vline2.addWidget(but2, alignment = Qt.AlignLeft)
+vline2.addWidget(label7, alignment = Qt.AlignLeft)
+vline2.addWidget(but3, alignment = Qt.AlignLeft)
+vline2.addWidget(line4, alignment = Qt.AlignLeft)
+vline2.addWidget(line5, alignment = Qt.AlignLeft)
+vline2.addWidget(start2, alignment = Qt.AlignCenter)
+
+layout1.addLayout(vline2)
+layout1.addLayout(vline3)
+win2.setLayout(layout1)
+
+
+start1.clicked.connect(toWin2)
+start2.clicked.connect(toWin3)
 
 win1.show()
 app.exec_()
