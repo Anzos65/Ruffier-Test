b97c1a8aaf9ca695af100871789315c391516842
Anzos65 <131809756+Anzos65@users.noreply.github.com>
2023-05-27 11:30:12 +0300

    Add files via upload

---
+++ func.py
@@ -0,0 +1,14 @@
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
+
+age = 0
+hr1 = 0
+hr2 = 0
+hr3 = 0
+
+def check(line2, line3, line4, line5):
+    age=line2.text()
+    hr1=line3.text()
+    hr2=line4.text()
+    hr3=line5.text()
+    return [age, hr1, hr2, hr3]
