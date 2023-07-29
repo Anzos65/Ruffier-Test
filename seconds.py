1a136a536932c3b80f53e973874c74063a4fd326
Anzos65 <131809756+Anzos65@users.noreply.github.com>
2023-07-29 11:37:48 +0300

    Add files via upload

--- seconds.py
+++ seconds.py
@@ -6,15 +6,16 @@ class Seconds(Label):
     done = BooleanProperty(False)
     def __init__(self, total, **kwargs):
         self.done = False
-        self.total = 0
+        self.total = total
         self.current = 0
         my_txt = "Seconds passed: " + str(self.current) 
         super().__init__(text = my_txt)
+
     def start(self):
         Clock.schedule_interval(self.change, 1)
+
     def change(self, dt):
         self.current += 1
-        print(self.current)
         self.text = "Seconds passed: " + str(self.current)
         if self.current >= self.total:
             self.done = True
